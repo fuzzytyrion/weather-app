44c5205b79339ccaa2d110908ae0e80cf3e07172
fuzzytyrion <matthewrdennis@gmail.com>
2018-02-21 07:21:36 +0000

    add command line arguments

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to get weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv.address);
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyADA_yTBbzkAwB8k7UxHqVHoMf3EYAhf3A',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyADA_yTBbzkAwB8k7UxHqVHoMf3EYAhf3A`,
     json: true
 }, (error, response, body) => {
     console.log(`Address ${body.results[0].formatted_address}`);
